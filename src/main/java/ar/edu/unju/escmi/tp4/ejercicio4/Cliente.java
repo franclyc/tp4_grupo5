a7c38ee21ec20cb5818438ea62db12793151f5e0
RichetOct2410 <octavioricci60@icloud.com>
2026-09-06 18:04:18 -0300

    Ejercicio 4 Terminado. Octavio Ricci.

---
+++ src/main/java/ar/edu/unju/escmi/tp4/ejercicio4/Cliente.java
@@ -0,0 +1,16 @@
+package ar.edu.unju.escmi.tp4.ejercicio4;
+public class Cliente {
+    private int dni;
+    private String nombre;
+    private String tipoCliente;
+    public Cliente(int dni, String nombre, String tipoCliente) {
+        this.dni=dni;
+        this.nombre=nombre;
+        this.tipoCliente=tipoCliente;}
+    public int getDni() {
+        return dni;}
+    public String getNombre() {
+        return nombre;}
+    public String getTipoCliente() {
+        return tipoCliente;
+    }}
